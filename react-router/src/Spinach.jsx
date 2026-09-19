54a0337e4c451b142589e2a568af3f8318d4fad1
Esguardian12 <esmultipro@gmail.com>
2026-09-19 15:35:05 -0400

    I added and modified the files to complete the assignment.

--- react-router/src/Spinach.jsx
+++ react-router/src/Spinach.jsx
@@ -8,3 +8,5 @@ const Spinach = () => {
     </>
   );
 };
+
+export default Spinach;
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -3,28 +3,23 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
 import Profile from "./Profile";
-import DefaultProfile from "./DefaultProfile";
-import Spinach from "./Spinach";
-import Popeye from "./Popeye"; 
+import ErrorPage from "./ErrorPage";
+import routes from "./routes";
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: "profile",
-    element: <Profile/>,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye />},
-    ],
+    path: "profile/:name",
+    element: <Profile />,
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
