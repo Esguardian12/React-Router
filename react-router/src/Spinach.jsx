ab9fe04adfcf270b89ce840f43c2d78bfa95dbd9
Esguardian12 <esmultipro@gmail.com>
2026-09-19 14:00:25 -0400

    I added the necessary files, ran into a problem but will fix it.

---
+++ react-router/src/Spinach.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+const Spinach = () => {
+    return (
+    <>
+        <p>Hi, I am SPinach! Popeye loves to est me!</p>
+        <link to="/">Click here to go back</link>
+    </>
+  );
+};
